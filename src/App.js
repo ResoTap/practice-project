05bb6453f69d23465792ddc3fe38fc222c97bd74
Patrick Oser <patrick.oser148@gmail.com>
2023-11-20 15:04:13 -0500

    Smll change.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './header';
 import Content from './content';
+import Footer from './footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Content />
+      <Footer />
     </div>
   );
 }
