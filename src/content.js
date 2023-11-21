74f32993e32533f9b54fb9b0f43ab7a44e30d3ba
Patrick Oser <patrick.oser148@gmail.com>
2023-11-21 11:51:02 -0500

    reset conent file for new chapter.

--- src/content.js
+++ src/content.js
@@ -1,32 +1,11 @@
 import { useState } from 'react';
 
 const Content = () => {
-    const [name, setName] = useState('Pat');
-    const [count, setCount] = useState(0);
-
-    const handleNameChange = () => {
-        const names = ['Bob', 'Kevin', 'Pat'];
-        const int = Math.floor(Math.random() * 3);
-        setName(names[int]);
-      }
-
-      const handleClick = () => {
-        setCount(count + 1)
-        console.log(count)
-      }
-
-      const handleClick2 = () => {
-        console.log(count)
-      }
+    const [items, setItems] = useState('Pat');
 
     return (
         <main>
-            <p onDoubleClick={handleClick}>
-                Hello {name}!
-            </p>
-            <button onClick={handleNameChange}>Change Name</button>
-            <button onClick={handleClick}>Click It</button>
-            <button onClick={handleClick2}>Click It</button>
+
         </main>
     )
 }
