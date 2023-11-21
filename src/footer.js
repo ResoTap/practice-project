0c1a2c6561fceba6c3bcf4836002e964e64f280d
Patrick Oser <patrick.oser148@gmail.com>
2023-11-21 15:12:32 -0500

    Add count of items in list to footer.

--- src/footer.js
+++ src/footer.js
@@ -1,10 +1,12 @@
-const Footer = () => {
+
+
+const Footer = ({ length }) => {
     const today = new Date();
 
     return (
         <footer>
             <p>
-                Copyright &copy; {today.getFullYear()}
+                {length} List { length ===1 ? 'Item' : 'Items' }
             </p>
         </footer>
     )
