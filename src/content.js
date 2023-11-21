4df421e17f3206ea07fa26ac66e190ab936fe643
Patrick Oser <patrick.oser148@gmail.com>
2023-11-21 13:56:48 -0500

    Add message to display when list is empty.

--- src/content.js
+++ src/content.js
@@ -27,22 +27,28 @@ const Content = () => {
     }
 
     const handleDelete = (id) => {
-      console.log(id)
+      const listItems =items.filter((item) => item.id !== id)
+      setItems(listItems);
+      localStorage.setItem('shoppinglist', JSON.stringify(listItems));
     }
 
     return (
         <main>
-          <ul>
-            {items.map((item) => (
-              <li className='item' key={item.id}>
-                <input type="checkbox" onChange={() => handleCheck(item.id)} checked={item.checked} />
-               <label style={(item.checked) ? { textDecoration: 'line-through'} : null} onDoubleClick={() => handleCheck(item.id)}>
-                {item.item}
-               </label>
-               <FaTrashAlt role='button' tabIndex='0' />
-              </li>
-            ))}
-          </ul>
+          {items.length ? (
+            <ul>
+              {items.map((item) => (
+                <li className='item' key={item.id}>
+                  <input type="checkbox" onChange={() => handleCheck(item.id)} checked={item.checked} />
+                <label style={(item.checked) ? { textDecoration: 'line-through'} : null} onDoubleClick={() => handleCheck(item.id)}>
+                  {item.item}
+                </label>
+                <FaTrashAlt onClick={() => handleDelete(item.id)} role='button' tabIndex='0' />
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p style={{ margin: '2rem' }}>Your list is empty</p>
+          )}
         </main>
     )
 }
