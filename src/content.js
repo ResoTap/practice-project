85616886b89f2fad55daf82812c90d74c1e51425
Patrick Oser <patrick.oser148@gmail.com>
2023-11-21 11:55:07 -0500

    Add array to useState.

--- src/content.js
+++ src/content.js
@@ -1,7 +1,24 @@
 import { useState } from 'react';
 
 const Content = () => {
-    const [items, setItems] = useState('Pat');
+    const [items, setItems] = useState([
+      {
+        id: 1,
+        checked: false,
+        item: "One half pound bag of Covered"
+      },
+      {
+        id: 2,
+        checked: false,
+        item: "Item 2"
+      },
+      {
+        id: 3,
+        checked: false,
+        item: "Item 3"
+      }
+    ]);
+
 
     return (
         <main>
