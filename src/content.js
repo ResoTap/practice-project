5e2e9311e1c6caf399dc1f3ac753712b4017e2d8
Patrick Oser <patrick.oser148@gmail.com>
2023-11-21 15:22:45 -0500

    Minor fomrat change.

--- src/content.js
+++ src/content.js
@@ -3,17 +3,17 @@ import ItemList from './itemList';
 const Content = ({ items, handleCheck, handleDelete}) => {
 
   return (
-      <main>
-        {items.length ? (
-          <ItemList
-            items={items}
-            handleCheck={handleCheck}
-            handleDelete={handleDelete}
-          />
-        ) : (
-          <p style={{ margin: '2rem' }}>Your list is empty</p>
-        )}
-      </main>
+    <main>
+      {items.length ? (
+        <ItemList
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
+      ) : (
+        <p style={{ margin: '2rem' }}>Your list is empty</p>
+      )}
+    </main>
   )
 }
 
