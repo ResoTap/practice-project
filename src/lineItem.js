c63f17b95af3106dea0d7efd70e15aca1214d0ca
Patrick Oser <patrick.oser148@gmail.com>
2023-11-21 16:54:19 -0500

    Remove repeated code.

--- src/lineItem.js
+++ src/lineItem.js
@@ -3,7 +3,7 @@ import { handleCheck, handleDelete } from './App'
 
 const LineItem = ({ item, handleCheck, handleDelete }) => {
     return (
-        <li className='item' key={item.id}>
+        <li className='item'>
                 <input type="checkbox" onChange={() => handleCheck(item.id)} 
                 checked={item.checked} 
             />
