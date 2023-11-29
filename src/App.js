3f15a34f05a75d064ef5554ba3a76016b4a6108d
Patrick Oser <patrick.oser148@gmail.com>
2023-11-29 13:19:31 -0500

    Fetch data through api.

--- src/App.js
+++ src/App.js
@@ -6,14 +6,30 @@ import Footer from './footer';
 import { useState, useEffect } from 'react';
 
 function App() {
+  const API_URL = 'http://localhost:3500/itemss';
 
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || []);
+  const [items, setItems] = useState([]);
   const [newItem, setNewItem] = useState('')
   const [search, setSearch] = useState('')
+  const [fetchError, setFetchError] = useState(null);
 
 useEffect(() => {
-  localStorage.setItem('shoppinglist', JSON.stringify(items))
-}, [items])
+  
+  const fetchItems = async () => {
+    try {
+      const response = await fetch(API_URL);
+      if (!response.ok) throw Error('Did not receive expected data');
+      const listItems = await response.json();
+      console.log(listItems);
+      setItems(listItems)
+      setFetchError(null);
+    } catch (err) {
+      setFetchError(err.message)
+    }
+  }
+  (async () => await fetchItems())();
+
+}, [])
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
@@ -52,11 +68,13 @@ useEffect(() => {
         search={search}
         setSearch={setSearch}
       />
-      <Content 
-        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-      />
+      <main>
+        <Content 
+          items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
+      </main>
       <Footer length={items.length} />
     </div>
   );
